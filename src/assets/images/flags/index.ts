a5fe739b75dda13f84b1cb849d3d2cfd80b96961
Daniil Trusov <daniiltrusov00@gmail.com>
2025-10-08 13:50:07 +0300

    update images

---
+++ src/assets/images/flags/index.ts
@@ -0,0 +1,5 @@
+export { default as GermanyFlagImg } from './de.png';
+export { default as SpainFlagImg } from './es.png';
+export { default as FranceFlagImg } from './fr.png';
+export { default as ItalyFlagImg } from './it.png';
+export { default as NetherlandsFlagImg } from './nl.png';
---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as StripeImg1 } from './img1.jpg';
+export { default as StripeImg2 } from './img2.jpg';
+export { default as StripeImg3 } from './img3.jpg';
+export { default as StripeLogoImg } from './stripe_logo.png';
